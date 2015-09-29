e12fb6b3dcad4d9f0927a89669f64b06af9fc7f1
TakaiMasatomo <masatomo@takai.mobi>
2015-09-29 14:47:07 -0700

    modify create sql

--- DB/create_database.sql
+++ DB/create_database.sql
@@ -1,2 +1,2 @@
-CREATE database spring-batch CHARACTER SET utf8;
+CREATE database spring_batch CHARACTER SET utf8;
 GRANT ALL ON spring_batch.* TO springbatchuser@'localhost' IDENTIFIED BY 'springbatchuser';
